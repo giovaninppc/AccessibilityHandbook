e4f7b5f4d33cbc6df467699a938904e1771e84bf
Giovani Pereira <giovani.nascimento@faire.com>
2022-09-20 18:08:57 -0300

    Initial Commit

---
+++ Accessibility Handbook/Accessibility_HandbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessibility_HandbookApp.swift
+//  Accessibility Handbook
+//
+//  Created by Giovani Nascimento Pereira on 20/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Accessibility_HandbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
