1d8fa3c2958f150bb15ca08bcc591ce5708a88e0
Giovani Pereira <giovani.nascimento@faire.com>
2022-09-21 09:28:16 -0300

    Finished Find The Password

--- Accessibility Handbook/Game/Puzzles/FindThePassword.swift
+++ Accessibility Handbook/Game/Puzzles/FindThePassword.swift
@@ -16,21 +16,78 @@ struct FindThePassword: GamePage, View {
   @State var success: Bool = false
   @State var hinting: Bool = false
 
+  @State private var word: String = ""
+  @FocusState private var fieldIsFocused: Bool
+
+  var hasSucceeded: Bool {
+    word == "46197"
+  }
+
   var body: some View {
     GameContent(success: $success, hinting: $hinting) {
       VStack(alignment: .leading, spacing: .regular) {
         Spacer()
           .frame(height: .large)
         Text("You need to type in the correct password!")
+          .accessibilitySortPriority(20)
         Text("It's as simple as that")
+          .accessibilitySortPriority(19)
         Comment("You just need to find out what the password is...")
+          .accessibilitySortPriority(18)
+        HStack {
+          item("1")
+            .accessibilitySortPriority(8)
+          item("2")
+            .accessibilityHidden(true)
+          item("3")
+            .accessibilityHidden(true)
+          item("4")
+            .accessibilitySortPriority(10)
+          item("5")
+            .accessibilityHidden(true)
+        }
+        HStack {
+          item("6")
+            .accessibilitySortPriority(9)
+          item("7")
+            .accessibilitySortPriority(6)
+          item("8")
+            .accessibilityHidden(true)
+          item("9")
+            .accessibilitySortPriority(7)
+          item("0")
+            .accessibilityHidden(true)
+        }
+        Spacer()
+          .frame(height: .regular)
+        TextField("Password", text: $word)
+          .keyboardType(.numberPad)
+          .focused($fieldIsFocused)
+          .padding()
+          .background(Color.secondaryBackground)
         Spacer()
       }
       .toAny()
     } link: {
       hintPage.toAny()
     }
+    .onChange(of: hasSucceeded) { newValue in
+      success = hasSucceeded
+      fieldIsFocused = false
+      if success {
+        GameStates.findThePasswordCompleted = true
+      }
+    }
   }
 
-  func hinted() {}
+  @ViewBuilder
+  func item(_ title: String) -> some View {
+    Text(title)
+      .font(.title2)
+      .padding()
+      .background {
+        RoundedRectangle(cornerRadius: 4.0)
+          .foregroundColor(.secondaryBackground)
+      }
+  }
 }
--- Accessibility Handbook/Models/GamePage.swift
+++ Accessibility Handbook/Models/GamePage.swift
@@ -10,15 +10,10 @@ import SwiftUI
 protocol GamePage: Page {
   var completed: Bool { get }
   var hintPage: AnyView { get }
-
-  func hinted()
 }
 
 extension GamePage where Self: View {
   var page: AnyView {
-    AnyView(
-      self
-        .navigationTitle(self.title)
-    )
+    self.navigationTitle(self.title).toAny()
   }
 }
