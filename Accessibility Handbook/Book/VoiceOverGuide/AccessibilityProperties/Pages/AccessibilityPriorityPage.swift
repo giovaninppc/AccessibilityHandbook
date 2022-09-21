8c89c113fb3de02f608291054b5020eab8c1eb51
Giovani Pereira <giovani.nascimento@faire.com>
2022-09-21 16:39:06 -0300

    Fix broken layout on priority view

--- Accessibility Handbook/Book/VoiceOverGuide/AccessibilityProperties/Pages/AccessibilityPriorityPage.swift
+++ Accessibility Handbook/Book/VoiceOverGuide/AccessibilityProperties/Pages/AccessibilityPriorityPage.swift
@@ -39,7 +39,7 @@ struct AccessibilityPriorityPage: View, Page {
           .accessibilitySortPriority(100)
         Text("It can be useful in some situations, check the example below:")
           .accessibilitySortPriority(100)
-        PriorityExampleView()
+        example
         Comment("Yes, you can change the reading order of your elements, but most of the time you dont want to do that.")
           .accessibilitySortPriority(-2)
         Comment("As a general guideline, we keep the reading order the same as the natural reading order of the current language")
@@ -54,10 +54,8 @@ struct AccessibilityPriorityPage: View, Page {
       .toAny()
     }
   }
-}
 
-private struct PriorityExampleView: View {
-  var body: some View {
+  var example: some View {
     VStack(spacing: .regular) {
       VerticalSpace(.large)
       HStack {
@@ -72,31 +70,30 @@ private struct PriorityExampleView: View {
         }
         .accessibilitySortPriority(-1)
       }
-      ScrollView {
-        HStack(spacing: .large) {
-          Image(systemName: "moon.fill")
-            .accessibilityLabel(Text("Moon"))
-            .accessibilitySortPriority(9)
-          Image(systemName: "cloud.fill")
-            .accessibilityLabel(Text("Cloud"))
-            .accessibilitySortPriority(8)
-          Image(systemName: "sun.max.fill")
-            .accessibilityLabel(Text("Sun"))
-            .accessibilitySortPriority(7)
-          Image(systemName: "sunrise.fill")
-            .accessibilityLabel(Text("Sunrise"))
-            .accessibilitySortPriority(6)
-          Image(systemName: "cloud.snow.fill")
-            .accessibilityLabel(Text("Snow"))
-            .accessibilitySortPriority(5)
-          Image(systemName: "cloud.bolt.rain.fill")
-            .accessibilityLabel(Text("Rain"))
-            .accessibilitySortPriority(4)
-          Image(systemName: "cloud.fog.fill")
-            .accessibilityLabel(Text("Fog"))
-            .accessibilitySortPriority(3)
-        }
+      HStack(spacing: .large) {
+        Image(systemName: "moon.fill")
+          .accessibilityLabel(Text("Moon"))
+          .accessibilitySortPriority(9)
+        Image(systemName: "cloud.fill")
+          .accessibilityLabel(Text("Cloud"))
+          .accessibilitySortPriority(8)
+        Image(systemName: "sun.max.fill")
+          .accessibilityLabel(Text("Sun"))
+          .accessibilitySortPriority(7)
+        Image(systemName: "sunrise.fill")
+          .accessibilityLabel(Text("Sunrise"))
+          .accessibilitySortPriority(6)
+        Image(systemName: "cloud.snow.fill")
+          .accessibilityLabel(Text("Snow"))
+          .accessibilitySortPriority(5)
+        Image(systemName: "cloud.bolt.rain.fill")
+          .accessibilityLabel(Text("Rain"))
+          .accessibilitySortPriority(4)
+        Image(systemName: "cloud.fog.fill")
+          .accessibilityLabel(Text("Fog"))
+          .accessibilitySortPriority(3)
       }
+      VerticalSpace(.large)
     }
     .padding()
   }
