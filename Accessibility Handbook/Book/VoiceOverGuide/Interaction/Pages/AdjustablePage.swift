1d9e52a30e0a8bec82e35e0eaf3e28e25491cd36
Giovani Pereira <giovani.nascimento@faire.com>
2022-09-21 17:50:57 -0300

    Added Adjustable trait

--- Accessibility Handbook/Book/VoiceOverGuide/Interaction/Pages/AdjustablePage.swift
+++ Accessibility Handbook/Book/VoiceOverGuide/Interaction/Pages/AdjustablePage.swift
@@ -10,12 +10,91 @@ import SwiftUI
 struct AdjustablePage: View, Page {
   var title: String = "Adjustable content"
 
+  let codeUIKit: String = """
+  accessibilityTraits = [.adjustable]
+
+  override func accessibilityIncrement() { ... }
+  override func accessibilityDecrement() { ... }
+  """
+
+  let codeSwiftUI: String = """
+  .accessibilityAdjustableAction { direction in
+      switch direction {
+      case .decrement:
+          currentValue -= 1
+      case .increment:
+          currentValue += 1
+      }
+  }
+  """
+
+  let link: String = """
+  https://developer.apple.com/documentation/uikit/uiaccessibilitytraits/1620177-adjustable
+  """
+
+  @State private var currentValue: Int = 0
+
   var body: some View {
     PageContent(next: CustomActionsPage()) {
       Group {
-
+        Text("Adjustable components are actually using an accessibility trait.")
+        InternalLink(page: ListOfAccessibilityTraitsPage().page, title: "See more on accessibility traits!")
+        Text("It's used on components that have some kind of quantity change or selection, like sliders, counters or quick-adds.")
+        Text("It's very powerful, and, when focused, an adjustable view will gain the swipe up and down gestures")
+        Text("That can be implemented to handle it's respective actions, without the need to directly tap into the buttons.")
+        example
+        Code(uiKit: codeUIKit, swiftUI: codeSwiftUI)
+        DocButton(link: link, title: title)
       }
       .toAny()
+    }
+  }
+
+  private var example: some View {
+    Group {
+      VerticalSpace(.regular)
+      HStack {
+        Spacer()
+        Button {
+          currentValue -= 1
+        } label: {
+          Image(systemName: "minus.circle.fill")
+            .resizable()
+            .frame(width: 30.0, height: 30.0)
+        }
+        .accessibilityHidden(true)
+
+        HorizontalSpace(.regular)
+
+        Text("\(currentValue)")
+          .font(.title.bold())
+
+        HorizontalSpace(.regular)
+
+        Button {
+          currentValue += 1
+        } label: {
+          Image(systemName: "plus.circle.fill")
+            .resizable()
+            .frame(width: 30.0, height: 30.0)
+        }
+        .accessibilityHidden(true)
+        Spacer()
+      }
+      .accessibilityElement(children: .combine)
+      .accessibilityValue("\(currentValue)")
+      .accessibilityAdjustableAction { direction in
+        switch direction {
+        case .decrement:
+          currentValue -= 1
+        case .increment:
+          currentValue += 1
+        @unknown default:
+          break
+        }
+      }
+
+      VerticalSpace(.regular)
     }
   }
 }
--- Accessibility Handbook/Book/VoiceOverGuide/Notifications/Pages/AnnouncementPage.swift
+++ Accessibility Handbook/Book/VoiceOverGuide/Notifications/Pages/AnnouncementPage.swift
@@ -22,6 +22,25 @@ struct AnnouncementPage: View, Page {
   https://developer.apple.com/documentation/uikit/uiaccessibility/notification/1620176-announcement
   """
 
+  @State private var currentValue: Int = 0
+
+  private var secretDescriptions: String {
+    switch currentValue {
+    case let value where value < 0:
+      return "Going negative, aren't we?"
+    case 0:
+      return ""
+    case 1...5:
+      return "Oh the values are increasing!"
+    case 6...10:
+      return "You like to increase, don't you?"
+    case 11...20:
+      return "Yep, keep increasing..."
+    default:
+      return "This is the final message"
+    }
+  }
+
   var body: some View {
     PageContent(next: nil) {
       Group {
@@ -30,16 +49,62 @@ struct AnnouncementPage: View, Page {
         Text("But, we cannot rely on visual cues to show information when talking about visual accessibility.")
         Comment("To me, that's where the announcements shine!")
         example
+        Comment("The code cells bellow are another example of announcements when you copy the code!")
         Code(uiKit: codeUIKit, swiftUI: codeSwiftUI)
         DocButton(link: link, title: title)
       }
       .toAny()
+    }
+    .onChange(of: secretDescriptions) { newValue in
+      UIAccessibility.post(notification: .announcement, argument: newValue)
     }
   }
 
   private var example: some View {
     Group {
       VerticalSpace(.regular)
+      HStack {
+        Spacer()
+        Button {
+          currentValue -= 1
+        } label: {
+          Image(systemName: "minus.circle.fill")
+            .resizable()
+            .frame(width: 30.0, height: 30.0)
+        }
+        .accessibilityHidden(true)
+
+        HorizontalSpace(.regular)
+
+        Text("\(currentValue)")
+          .font(.title.bold())
+
+        HorizontalSpace(.regular)
+
+        Button {
+          currentValue += 1
+        } label: {
+          Image(systemName: "plus.circle.fill")
+            .resizable()
+            .frame(width: 30.0, height: 30.0)
+        }
+        .accessibilityHidden(true)
+        Spacer()
+      }
+      .accessibilityElement(children: .combine)
+      .accessibilityValue("\(currentValue)")
+      .accessibilityAdjustableAction { direction in
+        switch direction {
+        case .decrement:
+          currentValue -= 1
+        case .increment:
+          currentValue += 1
+        }
+      }
+
+      VerticalSpace(.regular)
+      Text(secretDescriptions)
+        .font(.callout.bold())
       VerticalSpace(.regular)
     }
   }
